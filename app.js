fb374df50fdf30ab13e60c64290b38e6f0a45cc7
Nipun Fernando <nipunjayanga@gmail.com>
2019-12-16 09:20:40 +0000

    Create new campground feature added

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+var campgrounds = [
+		{name: "salmon creek", image: "https://www.photosforclass.com/download/pixabay-691424?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F50e9d4474856b108f5d084609620367d1c3ed9e04e50744e732f7adc944fcc_960.jpg&user=Free-Photos"},
+		{name: "granite hill", image: "https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c72297bd59f4ec751_960.jpg&user=Pexels"},
+		{name: "dawsons creek", image: "https://www.photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c72297bd59f4ec751_960.jpg&user=chanwity"},
+	];
+
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "salmon creek", image: "https://www.photosforclass.com/download/pixabay-691424?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F50e9d4474856b108f5d084609620367d1c3ed9e04e50744e732f7adc944fcc_960.jpg&user=Free-Photos"},
-		{name: "granite hill", image: "https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c72297bd59f4ec751_960.jpg&user=Pexels"},
-		{name: "dawsons creek", image: "https://www.photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c72297bd59f4ec751_960.jpg&user=chanwity"},
-	];
-	
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
 
@@ -24,7 +24,11 @@ app.get("/campgrounds/new", function(req, res){
 });
 
 app.post("/campgrounds", function(req, res){
-	res.send("you hit the post route");
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	res.redirect("/campgrounds");
 });
 
 
